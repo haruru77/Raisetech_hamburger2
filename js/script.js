0cb3f7fb2ab863f94ae43300e84cdd89c106eaba
haruru <teru.o.77@gmail.com>
2020-07-23 10:56:49 -0400

    responsive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-$(".p-sidemenu").click(function() {
+$(".p-sidebar").click(function() {
     // $(this).toggleClass("on");
-    $(".l-aside").slideToggle();
-    $(".l-nav-close").slideToggle();
+    $(".p-sidebar__menu__wrap").toggleClass('is-active');
+    $(".l-nav-close").toggleClass('is-active');
+    $(".p-sidebar__back").toggleClass('is-active');
   });
